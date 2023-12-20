70de729ed8659f151d9a2120feca297b5ca17ae3
Dayananda-Laishram <150163545+Dayananda-Laishram@users.noreply.github.com>
2023-12-20 15:49:49 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -4,9 +4,9 @@ int main()
  char oper;
  int no1,no2,result;
  printf("Enter the value of no1 \n");
- scanf("%d",&no1);
+ scanf("%d",no1);
  printf("enter the value of no2 \n");
- scanf("%d",&no2);
+ scanf("%d",no2);
  printf("what operation would you like to operate ?");
  printf("1. for addition type a\n 2. for substraction type b");
  scanf("%c",oper);
