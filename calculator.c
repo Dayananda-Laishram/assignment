5f966f286adc276443beddfbbaa48dc4618bf16f
Dayananda-Laishram <150163545+Dayananda-Laishram@users.noreply.github.com>
2023-12-21 06:58:05 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,25 +1,33 @@
-#include<stdio.h>
-int main()
-{
- char oper;
- int no1,no2,result;
- printf("Enter the value of no1 \n");
- scanf("%d",no1);
- printf("enter the value of no2 \n");
- scanf("%d",no2);
- printf("what operation would you like to operate ?");
- printf("1. for addition type a\n 2. for substraction type b");
- scanf("%c",oper);
- switch(oper)
-{
-	case a;
-	result=no1+no2;
-	break;
-	case b;
-	result=no1-no2;
-	break;
+#include <stdio.h>
 
-}
-return 0;
-}
+int main() {
+    char oper;
+    int no1, no2, result;
 
+    printf("Enter the value of no1: ");
+    scanf("%d", &no1);
+
+    printf("Enter the value of no2: ");
+    scanf("%d", &no2);
+
+    printf("What operation would you like to perform?\n");
+    printf("1. for addition type 'a'\n2. for subtraction type 'b'\n");
+    
+    // Use a space before %c in scanf to skip any whitespace characters (like newline) left in the input buffer.
+    scanf(" %c", &oper);
+
+    switch (oper) {
+        case 'a':
+            result = no1 + no2;
+            printf("Result of addition: %d\n", result);
+            break;
+        case 'b':
+            result = no1 - no2;
+            printf("Result of subtraction: %d\n", result);
+            break;
+        default:
+            printf("Invalid operation code entered.\n");
+    }
+
+    return 0;
+}
